62beef6f719bebd0b31ab1823202a7e3b23d09f9
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-01-19 21:24:38 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, 'client/build');
+
+// Setup static directory to serve
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(publicDirectoryPath));
+  app.get('*', (req, res) => {
+    req.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+  });
+}
+
+app.listen(port, (e) => {
+  if (e) return console.log(e);
+  console.log('Server running on port + ' + port);
+});
