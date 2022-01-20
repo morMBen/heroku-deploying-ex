aa64b45feded498acba5c9ca6e9fbc740ee65e65
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-01-20 14:38:12 +0200

    testing

--- app.js
+++ app.js
@@ -8,11 +8,18 @@ const port = process.env.PORT || 5000;
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, 'client/build');
 
-app.use(cors());
-// Setup static directory to serve
+// app.use(cors());
 app.use(express.static(publicDirectoryPath));
-// if (process.env.NODE_ENV === 'production') {
-// }
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.get('/api/users', (req, res) => {
   try {
