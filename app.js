c1f50ba54625c0b3634c2df26d7d1dbb3cf89ead
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2022-01-20 14:17:10 +0200

    test

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const app = express();
-
+const cors = require('cors');
 const path = require('path');
+
+const app = express();
 
 const port = process.env.PORT || 5000;
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, 'client/build');
 
+app.use(cors());
 // Setup static directory to serve
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(publicDirectoryPath));
@@ -14,6 +16,14 @@ if (process.env.NODE_ENV === 'production') {
     req.sendFile(path.resolve(__dirname, 'client/build/index.html'));
   });
 }
+
+app.get('/api/users', (req, res) => {
+  try {
+    res.status(200).send({ userName: 'Bob' });
+  } catch (e) {
+    res.status(400).send({ error: e.message });
+  }
+});
 
 app.listen(port, (e) => {
   if (e) return console.log(e);
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import myApi from './api/api';
 
 function App() {
-  return <div className='App'>Alor Dalor</div>;
+  const [user, setUser] = useState('');
+  const getReq = async () => {
+    const { data } = await myApi.get('users');
+    setUser(data.userName);
+    console.log(data.userName);
+  };
+  return (
+    <div className='App'>
+      <h1>Alor Dalor</h1>
+      <button onClick={getReq}>Get req</button>
+      <h3>{user}</h3>
+    </div>
+  );
 }
 
 export default App;
